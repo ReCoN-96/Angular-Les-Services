06278bda00d1c4afc24476e1ffc439eac9576a57
ReCon <osf@hotmail.fr>
2019-06-03 17:34:30 +0200

    Angular service

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CocktailListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,30 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailService {
+
+  cocktails = [
+    {
+      name: 'Mojito',
+      price: '7€',
+      img: 'https://zupimages.net/up/19/15/4j3c.jpeg'
+    },
+    {
+      name: 'Ginto',
+      price: '10€',
+      img: 'https://zupimages.net/up/19/15/oaei.jpg'
+    },
+    {
+      name: 'Vodka Orange',
+      price: '5€',
+      img: 'https://zupimages.net/up/19/15/jkv4.jpg'
+    }
+  ];
+
+  getCocktails() {
+    return this.cocktails;
+  }
+}
